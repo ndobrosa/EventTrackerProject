48d2199d9b026779a4c9a871d1697d268a1cc590
ndobrosa <ndobrosa@gmail.com>
2018-11-10 11:06:36 -0700

    Rest stubbed out

---
+++ PetOwnerRest/src/main/java/com/skilldistillery/petowner/PetOwnerRestApplication.java
@@ -0,0 +1,12 @@
+package com.skilldistillery.petowner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetOwnerRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetOwnerRestApplication.class, args);
+	}
+}
